4f89a8343c2b83ae19a57fb43352615cc0434e6a
Amu <117264067+salmoenela@users.noreply.github.com>
2023-04-30 06:44:48 +0700

    do some updates
    * fixed undefined `cache`
    * import `DISCORD_TOKEN` using the `toObject()` function
    * reconnect if no close code is received

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+const { DISCORD_TOKEN } = Deno.env.toObject();
 connect();
 
 export function connect(cache) {
-  const ws = new WebSocket(cache.resume_gateway_url || "wss://gateway.discord.gg/?v=10&encoding=json");
-  ws.cache = cache || {};
+  const ws = new WebSocket(cache?.resume_gateway_url || "wss://gateway.discord.gg/?v=10&encoding=json");
+  ws.cache = cache || { seq: null };
 
   ws.onopen = function() {
     const identifyPayload = {
@@ -28,8 +29,8 @@ export function connect(cache) {
     const resumeableOpcodes = [
       4000, 4001, 4002, 4003, 4005, 4007, 4008, 4009
     ];
-    if (resumeableOpcodes.includes(ctx.code)) {
-      connect(cache);
+    if (resumeableOpcodes.includes(ctx.code) || !ctx.code) {
+      connect(ws.cache);
     } else connect();
   }
 }
