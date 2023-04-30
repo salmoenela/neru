f66ab0075a5dd1cb0f0b4d54ca1baaa41916d330
Amu <117264067+salmoenela@users.noreply.github.com>
2023-04-30 15:48:41 +0700

    fixed `require-await` error
    https://github.com/salmoenela/neru/actions/runs/4843425794/jobs/8631035019#step:4:13

--- src/events/gateway/invalidSession.js
+++ src/events/gateway/invalidSession.js
@@ -2,7 +2,7 @@ import { connect, reconnect } from "../../index.js";
 
 export default {
   op: 9,
-  async execute(data) {
+  execute(data) {
     if (data.d) {
       reconnect(data.ws.cache);
     } else connect();
