aa62720d49084fb471844112d89d1297a124be7c
Amu <117264067+salmoenela@users.noreply.github.com>
2023-04-30 12:28:19 +0700

    define api version

--- src/events/dispatch/ready.js
+++ src/events/dispatch/ready.js
@@ -1,6 +1,7 @@
 export default {
   t: "READY",
   execute(data) {
+    data.ws.cache.v = data.d.v;
     data.ws.cache.session_id = data.d.session_id;
     data.ws.cache.resume_gateway_url = data.d.resume_gateway_url;
   }
