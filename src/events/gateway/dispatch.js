30de644e3a634942b69feb12be7c078c31d366d1
Amu <117264067+salmoenela@users.noreply.github.com>
2023-04-30 10:25:45 +0700

    change dispatch events handler

--- src/events/gateway/dispatch.js
+++ src/events/gateway/dispatch.js
@@ -1,7 +1,7 @@
 export default {
   op: 0,
   async execute(data) {
-    const event = data.ws.events.dispatch.find(ctx => ctx.t === data.t);
+    const event = Object.values(data.ws.events.dispatch).find(ctx => ctx.t === data.t);
     if (event) return await event.execute(data);
   }
 }
