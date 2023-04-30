18f1d763fc24f0352f73550e1a841d1da71f4c42
Amu <117264067+salmoenela@users.noreply.github.com>
2023-04-30 18:55:48 +0700

    tested supportedGuilds confess

--- src/commands/messages/confess.js
+++ src/commands/messages/confess.js
@@ -6,7 +6,7 @@ export default {
   },
   async execute(message) {
     const supportedGuilds = [
-      { guildId: "", targetChannel: "" }
+      { guildId: "1021724318767517696", targetChannel: "1100426483756957786" }
     ];
     const confess = supportedGuilds.find(ctx => ctx.guildId === message.guild_id);
 
@@ -23,6 +23,18 @@ export default {
         })
       });
     } else {
+      if (!message.args.length) return await fetch(`https://discord.com/api/v${message.ws.cache.v}/channels/${message.channel_id}/messages`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bot ${Deno.env.get("DISCORD_TOKEN")}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          content: `Kata kata confess nya apa bang messi?`,
+          message_reference: { guild_id: message.guild_id, message_id: message.id }
+        })
+      });
+
       const icon_url = "https://cdn.discordapp.com/" + (message.member.avatar ? `guilds/${message.guild_id}/users/${message.author.id}/avatars/${message.member.avatar}.${message.member.avatar.startsWith("a_") ? "gif" : "png"}` : (message.author.avatar ? `avatars/${message.author.avatar}.${message.author.avatar.startsWith("a_") ? "gif" : "png"}` : `embed/avatars/${message.author.discriminator % 5}.png`));
       const confessMessage = await (await fetch(`https://discord.com/api/v${message.ws.cache.v}/channels/${confess.targetChannel}/messages`, {
         method: "POST",
