3fe08567ce0e014add32ddb6a2e7f79152b52e37
Amu <117264067+salmoenela@users.noreply.github.com>
2023-04-30 12:18:03 +0700

    fixed undefined property

--- src/events/dispatch/messageCreate.js
+++ src/events/dispatch/messageCreate.js
@@ -3,7 +3,7 @@ export default {
   async execute(data) {
     const message = data.d;
     if (message.content.startsWith(data.ws.config.prefix)) {
-      await fetch(`https://discord.com/api/v${data.ws.cache.v}/channels/${message.channelId}/messages`, {
+      await fetch(`https://discord.com/api/v${data.ws.cache.v}/channels/${message.channel_id}/messages`, {
         method: "POST",
         headers: {
           Authorization: `Bot ${Deno.env.get("DISCORD_TOKEN")}`,
