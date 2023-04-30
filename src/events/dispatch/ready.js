5eed56d43d611c18265d760554c19b4f0a0d1200
Amu <117264067+salmoenela@users.noreply.github.com>
2023-04-30 18:46:39 +0700

    create command handlers

--- src/events/dispatch/ready.js
+++ src/events/dispatch/ready.js
@@ -1,9 +1,18 @@
 export default {
   t: "READY",
-  execute(data) {
+  async execute(data) {
     const { v, user, resume_gateway_url, session_id } = data.d;
     data.ws.cache = {
       v, client: user, session_id, resume_gateway_url, seq: data.s
+    };
+
+    data.ws.commands = {};
+    for await (const dir of Deno.readDir("./src/commands")) {
+      data.ws.commands[dir.name] = {};
+      for await (const file of Deno.readDir(`./src/commands/messages`)) {
+        const command = (await import(`../../commands/${file.name}`)).default;
+        data.ws.commands[dir.name][file.name[0]] = command;
+      }
     }
   }
 }
