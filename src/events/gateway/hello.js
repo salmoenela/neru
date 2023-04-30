493b84054462331deb8bf2ca506b12573824d265
Noel <120244424+nutunzz@users.noreply.github.com>
2023-04-30 09:01:40 +0700

    define `intervalHeartbeat` as `WebSocket` property

--- src/events/gateway/hello.js
+++ src/events/gateway/hello.js
@@ -1,7 +1,7 @@
 export default {
   op: 10,
   execute(data) {
-    setInterval(() => {
+    data.ws.intervalHeartbeat = setInterval(() => {
       data.ws.send(JSON.stringify({
         op: 1,
         d: data.ws.cache.seq
