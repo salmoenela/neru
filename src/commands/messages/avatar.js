a08d14ad225dd6f0460cf16a6f4a01cd200bc2fd
Noel <120244424+nutunzz@users.noreply.github.com>
2023-05-01 08:34:01 +0700

    fix missed content-type header

--- src/commands/messages/avatar.js
+++ src/commands/messages/avatar.js
@@ -9,7 +9,8 @@ export default {
     const response = await fetch(`https://discord.com/api/v${message.ws.cache.v}/channels/${message.channel_id}/messages`, {
       method: "POST",
       headers: {
-        Authorization: `Bot ${Deno.env.get("DISCORD_TOKEN")}`
+        Authorization: `Bot ${Deno.env.get("DISCORD_TOKEN")}`,
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({
         content
