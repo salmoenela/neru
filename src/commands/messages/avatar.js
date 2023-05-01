39d103aedb66bdc854e87e91580c4ff1ae0f7da7
Noel <120244424+nutunzz@users.noreply.github.com>
2023-05-01 08:28:34 +0700

    create avatar command

---
+++ src/commands/messages/avatar.js
@@ -0,0 +1,19 @@
+export default {
+  data: {
+    name: "avatar",
+    description: "ᵎ⌇ Lihat avatar pengguna",
+    aliases: ["av"]
+  },
+  async execute(message) {
+    const content = "https://cdn.discordapp.com/" + (message.member.avatar ? `guilds/${message.guild_id}/users/${message.author.id}/avatars/${message.member.avatar}.${message.member.avatar.startsWith("a_") ? "gif" : "png"}` : (message.author.avatar ? `avatars/${message.author.id}/${message.author.avatar}.${message.author.avatar.startsWith("a_") ? "gif" : "png"}` : `embed/avatars/${message.author.discriminator % 5}.png`));
+    await fetch(`https://discord.com/api/v${message.ws.cache.v}/channels/${message.channel_id}/messages`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bot ${Deno.env.get("DISCORD_TOKEN")}`
+      },
+      body: JSON.stringify({
+        content
+      })
+    })
+  }
+}
