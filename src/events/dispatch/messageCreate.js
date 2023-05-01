181a8af59a6c43f808de4e765f805ad93af01d49
Amu <117264067+salmoenela@users.noreply.github.com>
2023-05-01 15:42:48 +0700

    feat: added aliases command support
    Co-authored-by: nutunzz <nutunzz@users.noreply.github.com>

--- src/events/dispatch/messageCreate.js
+++ src/events/dispatch/messageCreate.js
@@ -9,7 +9,7 @@ export default {
     if (message.content.startsWith(prefix)) {
       message.args = message.content.slice(prefix.length).trim().split(/ +/g);
       const commandName = message.args.shift();
-      const command = Object.values(data.ws.commands.messages).find(ctx => ctx.data.name === commandName);
+      const command = Object.values(data.ws.commands.messages).find(ctx => (ctx.data.name === commandName) || ctx.data.aliases.includes(commandName));
       if (command) return await command.execute(message);
     }
   }
