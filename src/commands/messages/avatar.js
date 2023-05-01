53fb64ba2ce1472fedae0e57197c9c1bf160c56d
Amu <117264067+salmoenela@users.noreply.github.com>
2023-05-01 15:41:08 +0700

    remove debugging function

--- src/commands/messages/avatar.js
+++ src/commands/messages/avatar.js
@@ -6,16 +6,16 @@ export default {
   },
   async execute(message) {
     const avatarURL = "https://cdn.discordapp.com/" + (message.member.avatar ? `guilds/${message.guild_id}/users/${message.author.id}/avatars/${message.member.avatar}.${message.member.avatar.startsWith("a_") ? "gif" : "png"}` : (message.author.avatar ? `avatars/${message.author.id}/${message.author.avatar}.${message.author.avatar.startsWith("a_") ? "gif" : "png"}` : `embed/avatars/${message.author.discriminator % 5}.png`)) + "?size=2048";
-    console.log(avatarURL)
     const avatarData = await fetch(avatarURL);
-    console.log(avatarData);
     const avatar = await avatarData.blob();
-    console.log(avatar);
+
     const body = new FormData();
+
     body.set("payload_json", JSON.stringify({
       message_reference: { message_id: message.id, guild_id: message.guild_id }
     }))
     body.set("files[0]", avatar, `avatar.${avatar.type.split("/")[1]}`);
+
     const response = await fetch(`https://discord.com/api/v${message.ws.cache.v}/channels/${message.channel_id}/messages`, {
       method: "POST",
       headers: {
@@ -23,6 +23,5 @@ export default {
       },
       body
     });
-    console.log((await response.json()));
   }
 }
