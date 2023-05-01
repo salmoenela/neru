b44a8449586ea8555864f8387dd8df6ad59f55f0
Amu <117264067+salmoenela@users.noreply.github.com>
2023-05-01 08:55:34 +0700

    added `payload_json` data

--- src/commands/messages/avatar.js
+++ src/commands/messages/avatar.js
@@ -6,7 +6,11 @@ export default {
   },
   async execute(message) {
     const avatar = await (await fetch("https://cdn.discordapp.com/" + (message.member.avatar ? `guilds/${message.guild_id}/users/${message.author.id}/avatars/${message.member.avatar}.${message.member.avatar.startsWith("a_") ? "gif" : "png"}` : (message.author.avatar ? `avatars/${message.author.id}/${message.author.avatar}.${message.author.avatar.startsWith("a_") ? "gif" : "png"}` : `embed/avatars/${message.author.discriminator % 5}.png`)) + "?size=2048")).body();
-    const body = new FormData().set("files[0]", avatar);
+    const body = new FormData()
+      .set("payload_json", JSON.stringify({
+        message_reference: { message_id: message.id, guild_id: message.guild_id }
+      }))
+      .set("files[0]", avatar);
     const response = await fetch(`https://discord.com/api/v${message.ws.cache.v}/channels/${message.channel_id}/messages`, {
       method: "POST",
       headers: {
