ad5ca51e099b59d748c094500f1545ff002c056b
Amu <117264067+salmoenela@users.noreply.github.com>
2023-05-01 08:52:02 +0700

    updated where the user avatar is placed

--- src/commands/messages/avatar.js
+++ src/commands/messages/avatar.js
@@ -5,16 +5,14 @@ export default {
     aliases: ["av"]
   },
   async execute(message) {
-    const content = "https://cdn.discordapp.com/" + (message.member.avatar ? `guilds/${message.guild_id}/users/${message.author.id}/avatars/${message.member.avatar}.${message.member.avatar.startsWith("a_") ? "gif" : "png"}` : (message.author.avatar ? `avatars/${message.author.id}/${message.author.avatar}.${message.author.avatar.startsWith("a_") ? "gif" : "png"}` : `embed/avatars/${message.author.discriminator % 5}.png`));
+    const avatar = await (await fetch("https://cdn.discordapp.com/" + (message.member.avatar ? `guilds/${message.guild_id}/users/${message.author.id}/avatars/${message.member.avatar}.${message.member.avatar.startsWith("a_") ? "gif" : "png"}` : (message.author.avatar ? `avatars/${message.author.id}/${message.author.avatar}.${message.author.avatar.startsWith("a_") ? "gif" : "png"}` : `embed/avatars/${message.author.discriminator % 5}.png`)) + "?size=2048")).body();
+    const body = new FormData().set("files[0]", avatar);
     const response = await fetch(`https://discord.com/api/v${message.ws.cache.v}/channels/${message.channel_id}/messages`, {
       method: "POST",
       headers: {
         Authorization: `Bot ${Deno.env.get("DISCORD_TOKEN")}`,
-        "Content-Type": "application/json"
       },
-      body: JSON.stringify({
-        content
-      })
+      body
     });
     console.log((await response.json()));
   }
